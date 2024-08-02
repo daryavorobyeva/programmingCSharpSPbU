e529036a556383d7d856f0ab3ba4057bd0ad4661
Dasha <vorobyeva.dasha1309@gmail.com>
2024-08-02 15:01:24 +0300

    Task: unique list

---
+++ UniqueList/UniqueList.Tests/ListTest.cs
@@ -0,0 +1,54 @@
+﻿namespace SimpleList.Tests;
+
+[TestClass()]
+public class ListTest
+{
+    List list;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        list = new List();
+    }
+
+    [TestMethod()]
+    public void AddTest()
+    {
+        list.Add(5);
+        list.Add(7);
+        Assert.AreEqual(2, list.Size);
+    }
+
+    [TestMethod()]
+    public void RemoveTestEmptyList()
+    {
+        var ex = Assert.ThrowsException<InvalidOperationException>(() => list.Remove());
+        Assert.AreEqual(ex.Message, "The list is empty.");
+    }
+
+    [TestMethod()]
+    public void RemoveTestOneElementInTheList()
+    {
+        list.Add(5);
+        list.Remove();
+        Assert.AreEqual(0, list.Size);
+    }
+
+    [TestMethod()]
+    public void RemoveTestMoreElementsInTheList()
+    {
+        list.Add(5);
+        list.Add(6);
+        list.Remove();
+        Assert.AreEqual(1, list.Size);
+    }
+
+    [TestMethod()]
+    public void ReplaceTestWrongPosition()
+    {
+        list.Add(5);
+
+        var ex = Assert.ThrowsException<IndexOutOfRangeException>(() => list.Replace(7, 10));
+        Assert.AreEqual(ex.Message, "Invalid item position number.");
+    }
+}
