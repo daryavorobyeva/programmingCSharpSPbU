1fbf38147e7f31b2b32086dea4e9a86a29acb96c
Dasha <vorobyeva.dasha1309@gmail.com>
2024-08-02 13:37:45 +0300

    Task: bubble sort

---
+++ BubbleSort/BubbleSort/Sorting.cs
@@ -0,0 +1,25 @@
+﻿namespace Sorting;
+
+public static class Sort
+{
+    /// <summary>
+    /// Sorts an array of integers using a bubble sorting algorithm.
+    /// </summary>
+    /// <param name="array"></param>
+    public static void BubbleSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    var temporaryVariable = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temporaryVariable;
+                }
+            }
+        }
+    }
+}
