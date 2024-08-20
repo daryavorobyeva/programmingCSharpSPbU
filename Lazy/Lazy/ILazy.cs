cd2b3bf188a819494db81370b58f5260d26c1c83
Dasha <vorobyeva.dasha1309@gmail.com>
2024-08-20 23:31:16 +0300

    Add 'out' keyword to parameter 'T'

--- Lazy/Lazy/ILazy.cs
+++ Lazy/Lazy/ILazy.cs
@@ -4,7 +4,7 @@
 /// Setting a T type element using lazy calculation.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public interface ILazy<T>
+public interface ILazy<out T>
 {
     /// <summary>
     /// Get a type T element using lazy calculation.
